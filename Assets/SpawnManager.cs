4494bdd35f6801aa059f5bf756e2ffbe1b8b5067
ArvinChau <89488993+ArvinChau@users.noreply.github.com>
2022-04-28 14:48:45 -0700

    finished all

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private PlayerController PlayerControllerScript;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // spawns an object in the scene
+    void SpawnObstacle()
+    {
+        if (PlayerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
